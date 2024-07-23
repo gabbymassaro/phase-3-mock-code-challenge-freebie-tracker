c7e6d90d3daed3fa684196f1ee051b02182e7cd2
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-23 08:36:12 -0400

    All but last method complete

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,8 @@ class Company < ActiveRecord::Base
   def give_freebie(dev, item_name, value)
     Freebie.create(item_name: item_name, value: value, dev: dev, company: self)
   end
+
+  def self.oldest_company
+    Company.order(founding_year: :asc).first
+  end
 end
